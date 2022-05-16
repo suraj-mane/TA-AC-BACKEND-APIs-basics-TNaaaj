f0915974cb82a3dd31421b6bb4c281e96e3b86f6
suraj-mane <sun.mane1100@gmail.com>
2022-05-16 16:47:47 +0530

    complete

---
+++ block-BNaaeh/book-store/model/book.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+var bookSchema = new schema({
+  title:{type:String},
+  cartgory:{type:String},
+  pages:{type:String},
+  price:{type:String},
+  commentId:{type:schema.Types.ObjectId, ref:"comment"}
+},{timestamps:true});
+
+module.exports = mongoose.model('book', bookSchema);
---
+++ block-BNaaeh/book-store/routes/bookv1.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var router = express.Router();
+var Book = require('../model/book');
+var Comment = require('../model/comment');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  Book.find({}, (err,books) =>{
+    if(err) return next(err);
+    res.status(200).json(books)
+  })
+});
+
+// get single book 
+router.get('/:id',(req,res,next) => {
+  var id = req.params.id;
+  Book.findById(id, (err,book) => {
+    if(err) return next(err);
+    res.status(200).json(book)
+  })
+})
+
+// Create book 
+router.post('/', (req,res,next) => {
+  Book.create(req.body, (err,book) => {
+    if(err) return next(err);
+    res.status(200).json(book);
+  })
+})
+
+// Update book 
+router.post('/:id', (req,res,next) => {
+  var id = req.params.id;
+  Book.findByIdAndUpdate(id,req.body,(err,book) => {
+    if(err) return next(err);
+    res.status(200).json(book)
+  })
+})
+
+// delete book 
+router.get('/:id', (req,res,next) => {
+  var id = req.params.id;
+  Book.findByIdAndDelete(id, (err,book) => {
+    if(err) return next(err);
+    res.status(200).json(book)
+  })
+})
+
+// Add comment 
+router.post('/:id/comment', (req,res,next) => {
+  var id = req.params.id;
+  req.body.bookId = id;
+  Comment.create(req.body, (err,comment) => {
+    if(err) return next(err);
+    Book.findByIdAndUpdate(id, {$push:{commentId:comment._id}}, (err,book) => {
+      if(err) return next(err);
+      res.status(200).json(book);
+    })
+  })
+})
+
+
+module.exports = router;
